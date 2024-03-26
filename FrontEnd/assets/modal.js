741d706528593b68370a01c2660f3675ff2479be
DEHBIA-HANI <129846731+DEHBIA-HANI@users.noreply.github.com>
2024-03-26 15:10:22 +0100

    Ajout en js :fermer la modale avec la touchetab du clavier

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,3 +1,4 @@
+// Tous les fonctions d'accessibilité pour les deux modales en même temps*/
 window.onload = () => {
   // On récupère les bouttons d'ouverture de modale
   const btnOuvertureModal = document.querySelectorAll("[data-toggle=modal]");
@@ -10,11 +11,17 @@ window.onload = () => {
       //On recupère la data-target(celle quand click dessus)
       const target = this.dataset.target;
 
-      //On recupère la  bonne modale
+      //On recupère la  bonne modale celle quand on clique dessus
       let modal = document.querySelector(target);
+
+      //On recupère les elements sur notre dom
       const modale1 = document.getElementById("modale1");
       const modale2 = document.getElementById("modale2");
       const modaleGlobale = document.querySelector(".modale");
+
+      const closeModal = function () {
+        modaleGlobale.style.display = "none";
+      };
       modaleGlobale.style.display = "flex";
       if (modal === modale1) {
         modale2.style.display = "none";
@@ -29,7 +36,7 @@ window.onload = () => {
       const modaleClose = document.querySelectorAll("[data-dismiss=dialog");
       for (let close of modaleClose) {
         close.addEventListener("click", () => {
-          modaleGlobale.style.display = "none";
+          closeModal();
         });
       }
       //On recupère le bouton de retour en arrière
@@ -40,7 +47,22 @@ window.onload = () => {
       });
 
       //on gère la fermeture lors du clic sur la zone grise
-      modaleGlobale.addEventListener("click", function (e) {});
+      modaleGlobale.addEventListener("click", function () {
+        closeModal();
+      });
+      //On évite la propagationdu clic d'un enfant à son parent
+      for (let i = 0; i < modaleGlobale.children.length; i++) {
+        modaleGlobale.children[i].addEventListener("click", function (e) {
+          e.stopPropagation();
+        });
+      }
+
+      window.addEventListener("keydown", function (e) {
+        //-1-On clic sur la touche "echap" du clavier pour fermer la modale
+        if (e.key === "Escape" || e.key === "Esc") {
+          closeModal(e);
+        }
+      });
     });
   }
 };
